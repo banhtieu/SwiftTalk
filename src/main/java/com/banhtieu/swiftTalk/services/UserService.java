b909c4b3301ff1db4e6240702dc477e77cc725d9
banhtieu <tuoitrevohoc@me.com>
2015-09-03 17:12:39 +0700

    + Add login & get current user

--- src/main/java/com/banhtieu/swiftTalk/services/UserService.java
+++ src/main/java/com/banhtieu/swiftTalk/services/UserService.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by banhtieu on 9/3/15.
  * The user service that handle all
@@ -22,6 +24,10 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private HttpSession session;
+    private User currentUser;
+
     /**
      * find all users in repository
      * @return
@@ -32,11 +38,66 @@ public class UserService {
     }
 
 
+    /**
+     * Logged in as a user
+     * @param accessToken
+     * @return
+     */
     @RequestMapping("/user/login")
-    public String login(@RequestParam String accessToken) {
+    public boolean login(@RequestParam String accessToken) {
+
+        org.springframework.social.facebook.api.User profile = getProfile(accessToken);
+
+        User user = userRepository.findByFacebookId(profile.getId());
+
+        if (user == null) {
+            user = new User();
+            user.setFacebookId(profile.getId());
+            user.setScreenName(profile.getName());
+
+            userRepository.save(user);
+        }
+
+        setCurrentUser(user);
+
+        return user != null;
+    }
+
+
+    /**
+     * Get the current user
+     * @return current logged in user
+     */
+    @RequestMapping("/user/me")
+    public User getCurrentUser() {
+        if (currentUser == null) {
+            try {
+                String userId = (String) session.getAttribute("currentUser");
+                currentUser = userRepository.findOne(userId);
+            } catch (Exception exception) {
+                currentUser = null;
+            }
+        }
+
+        return currentUser;
+    }
+
+    /**
+     * Get User Profile
+     * @param accessToken the facebook access token
+     * @return the facebook profile of logged in user
+     */
+    private org.springframework.social.facebook.api.User getProfile(String accessToken) {
         Facebook facebook = new FacebookTemplate(accessToken);
-        org.springframework.social.facebook.api.User user;
-        user = facebook.userOperations().getUserProfile();
-        return user.getName();
+        return facebook.userOperations().getUserProfile();
+    }
+
+    /**
+     * set the current user
+     * @param currentUser current user
+     */
+    protected void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+        session.setAttribute("currentUser", currentUser.getId());
     }
 }
