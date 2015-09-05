d18317888ce6b4d823ce6d7a72b49a5b79fb2812
banhtieu <tuoitrevohoc@me.com>
2015-09-05 11:04:40 +0700

    + login on load.

--- src/main/resources/static/Scripts/Services.js
+++ src/main/resources/static/Scripts/Services.js
@@ -27,19 +27,27 @@ service.factory('userService', ['$resource', function($resource){
 			});
 		},
 		login: function(){
-			FB.getLoginStatus(function(response){
-				if (response.status === 'connected'){
+			FB.login(function(response){
+				if (response.status === 'connected') {
 					userService.onLogin();
-				} else {
-					FB.login(function(response){
-						if (response.status === 'connected') {
-							userService.onLogin();
-						}
-					})
 				}
-			});
+			})
 		},
 		remote: new UserRemoteService()
+	};
+
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : '470932816419704',
+			xfbml      : true,
+			version    : 'v2.4'
+		});
+
+		FB.getLoginStatus(function(response){
+			if (response.status === 'connected'){
+				userService.onLogin();
+			}
+		});
 	};
 	
 	return userService;
